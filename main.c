a63ce077be242059cb96147875fa81f507c073ae
Vinmannen <82623760+Vinmannen@users.noreply.github.com>
2021-05-25 14:12:51 +0200

    Update main.c
    Comments to reminds myself what the heck this code means..

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main(int argc, char *argv[]){
-    char flag;
-    char *conv_from = "";
-    char *conv_to = "";
-    float value;
-
+    //Declaring variables
+    char flag; //Variable for getopt so it can be used in a switch
+    char *conv_from; //Pointer to the "from" character
+    char *conv_to; //Pointer to the "to" character
+    float value; //Conversion value
+    
+    //Some information about the usage
     puts("\n");
     puts("-f [char]     Temperature to convert from.");
     puts("-t [char]     Temperature to convert to.");
@@ -18,8 +19,10 @@ int main(int argc, char *argv[]){
     puts("Example: ./main -f f -t c -v 20\n");
 
 
-    while ((flag = getopt(argc, argv,"f:t:v:" )) != -1)
-        switch(flag){
+    //Read arguments while flag is not -1
+    //Go through each "optarg" and add it to the variables
+    while ((flag = getopt(argc, argv,"f:t:v:" )) != -1) //":" to make it required
+        switch(flag){ //a switch to handle each argument
             case 'f':
                 conv_from = optarg;
             case 't':
@@ -28,23 +31,34 @@ int main(int argc, char *argv[]){
                 value = atoi(optarg);
         }
 
+    //Skip past arguments to make option an option and input an input
     argc -= optind;
     argv += optind;
 
+    //If no, or an invalid, value entered, return an error
     if(!value){
         printf("Must enter a valid (v)alue (float or integer).\n");
         exit(1);
     }
 
+    //-f = f and -t = c do...
     if((*conv_from == 'f') && (*conv_to == 'c')){
         float f_to_c =  (value - 32) * .5556;
         printf("%1.2f°F is %1.2f°C\n", value, f_to_c);
     }
 
-    if((*conv_from == 'c') && (*conv_to == 'f')){
+    //-f = c and -t = f do...
+    else if((*conv_from == 'c') && (*conv_to == 'f')){
         float c_to_f =   (value * 1.8) + 32;
         printf("%1.2f°C is %1.2f°F\n", value, c_to_f);
-    } 
+    }
 
+    //If no valid value are given, return an error message
+    else{
+        printf("Invalid or unsupported value entered. Check your input and try again.\n");
+        exit(1);
+    }
+
+    //Standard return 0 if everything went OK
     return 0;
 }
